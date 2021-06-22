9919b895429318bd16f28d8e58b8b1c99a04a44c
Benoit Turcotte <benoiturcotte@gmail.com>
2021-06-22 09:14:08 -0400

    removed default seed

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 frame_shape = (64, 64)
 ensemble_func, synapses_list = utils.generate_synaptic_fibers(frame_shape, (9, 55), (3, 10), (2, 5),
-                                                              seed=155)
+                                                              seed=None)
 # Build a dictionnary corresponding synapses to a bool saying if they are currently flashing or not
 # They all start not flashing
 # flat_synapses_list = [item for sublist in synapses_list for item in sublist]
